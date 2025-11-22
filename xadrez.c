e5610a03f215679ace8f36bd032c08fcd723e657
Derion <117675047+derionzin@users.noreply.github.com>
2025-11-22 22:25:36 +0000

    nivel novato, uso de repetições

--- xadrez.c
+++ xadrez.c
@@ -8,14 +8,38 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+    const int bispo_passos = 5;
+    const int torre_passos = 5;
+    const int rainha_passos = 8;
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("bispo: \n");
+    for (int i = 0; i < bispo_passos; i++) {
+        printf("Cima\n");
+        printf("Direita\n");
+    }
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    printf("torre: \n");
+    int j = 0;
+    while (j < torre_passos) {
+        printf("direita \n");
+        j++
+    }
+
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
+    printf("rainha: \n");
+    int h = 0;
+    do {
+        printf("esquerda \n");
+        h++
+    } while (h < rainha_passos);
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
